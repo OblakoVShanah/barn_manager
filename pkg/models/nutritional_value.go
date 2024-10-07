8414fc297a5e67431ebaa6b311b594dde1f57da7
Nikolai Komarov <komarov1845@gmail.com>
2024-10-07 19:52:22 +0300

    [Feat] Added error handling for FoodProduct methods

--- pkg/models/nutritional_value.go
+++ pkg/models/nutritional_value.go
@@ -1,6 +1,6 @@
 package models
 
-// Nutritional value on 100 gramms of product
+// A NutritionalValueRelative represents a nutritional value per 100g of a product
 type NutritionalValueRelative struct {
 	Proteins      int `json:"proteins"`
 	Fats          int `json:"fats"`
@@ -8,7 +8,7 @@ type NutritionalValueRelative struct {
 	Calories      int `json:"calories"`
 }
 
-// Nutritional value of whole product
+// NutritionalValueAbsolute represents a nutritional value of a whole product
 type NutritionalValueAbsolute struct {
 	Proteins      int `json:"proteins"`
 	Fats          int `json:"fats"`
@@ -16,6 +16,8 @@ type NutritionalValueAbsolute struct {
 	Calories      int `json:"calories"`
 }
 
+// AddAbsoluteValue adds absolute nutritional value to existed NutritionalValueAbsolute and
+// returns new instance of NutritionalValueAbsolute.
 func (nv_left NutritionalValueAbsolute) AddAbsoluteValue(nv_right NutritionalValueAbsolute) NutritionalValueAbsolute {
 	return NutritionalValueAbsolute{
 		Proteins:      nv_left.Proteins + nv_right.Proteins,
@@ -25,6 +27,8 @@ func (nv_left NutritionalValueAbsolute) AddAbsoluteValue(nv_right NutritionalVal
 	}
 }
 
+// AddRelativeValue adds relative nutritional value multiplied by weight to existed NutritionalValueAbsolute and
+// returns new instance of NutritionalValueAbsolute.
 func (nv_left NutritionalValueAbsolute) AddRelativeValue(nv_right NutritionalValueRelative, weight_right float32) NutritionalValueAbsolute {
 	return NutritionalValueAbsolute{
 		Proteins:      nv_left.Proteins + int(float32(nv_right.Proteins)*weight_right),
